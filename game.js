042f3fb94b4bc9566389a66b8681d70f161fdc41
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-13 16:19:28 -0500

    feat: make function pick rock paper or scissors
    Since there are 3 choices, split 1.0 into 3 parts. Create an if/else if
    statement for every part, so 0.01-0.33, 0.34-0.66, and 0.67+.
    Give every if/else if statement a different value for computerChoice.
    This makes the function pick randomly between rock, paper, and scissors.

--- game.js
+++ game.js
@@ -7,12 +7,19 @@ function getComputerChoice() {
     console.log(`randomNumber is ${randomNumber}.`)
     // GENERATE a random number with Math.random
 
+    if (randomNumber >= 0.67) {
+        computerChoice = "Scissors";
+    } else if (randomNumber <= 0.66 && randomNumber > 0.33) {
+        computerChoice = "Paper";
+    } else if (randomNumber <= 0.33 && randomNumber > 0.01) {
+        computerChoice = "Rock";
+    }
 
+    console.log(`computerChoice is ${computerChoice}.`)
         // IF 0.01 or more make computerChoice "rock"
         // IF 0.34 or more make computerChoice "paper"
         // IF 0.67 or more make computerChoice "scissors"
-        
-    console.log(`computerChoice is ${computerChoice}.`)
+
 }
 
 getComputerChoice();
