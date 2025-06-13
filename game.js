7ccd3a44e87c5f51b17e12778aeaae8b556e9b96
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-13 15:13:01 -0500

    refractor: add pseudocode for computer's choice

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-console.log("It works again!")
+// INIT a function called getComputerChoice
+// INIT a variable called computerChoice
+// IF 0.01 or more make computerChoice "rock"
+// IF 0.34 or more make computerChoice "paper"
+// IF 0.67 or more make computerChoice "scissors"
+// ELSE loop until another number is provided
+// RETURN computerChoice
