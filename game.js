84bf77e2c4df83b3d240e838b9ed4ef41e8e36ae
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-13 16:05:06 -0500

    refractor: create variables and an empty function
    There is a variable to hold the computer's choice and a variable
    to hold the random number. computerChoice was a necessary variable, but
    the randomNumber variable was made because it allows for the
    Math.random value to be read. This is important for troubleshooting
    with console.log.

--- game.js
+++ game.js
@@ -1,7 +1,22 @@
 // INIT a function called getComputerChoice
-// INIT a variable called computerChoice
-// IF 0.01 or more make computerChoice "rock"
-// IF 0.34 or more make computerChoice "paper"
-// IF 0.67 or more make computerChoice "scissors"
+function getComputerChoice() {
+    let computerChoice;
+    // INIT a variable called computerChoice
+
+    randomNumber = Math.random()
+    console.log(`randomNumber is ${randomNumber}.`)
+    // GENERATE a random number with Math.random
+
+
+        // IF 0.01 or more make computerChoice "rock"
+        // IF 0.34 or more make computerChoice "paper"
+        // IF 0.67 or more make computerChoice "scissors"
+        
+    console.log(`computerChoice is ${computerChoice}.`)
+}
+
+getComputerChoice();
+
+
 // ELSE loop until another number is provided
 // RETURN computerChoice
