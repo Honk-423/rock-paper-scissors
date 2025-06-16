6a4cfcdfa50b592ebae815665c2a25f0c83c35b1
Talia <139439068+Honk-423@users.noreply.github.com>
2025-06-16 15:19:48 -0500

    refractor: remove lowercase func from humanchoice
    Remove the code that makes humanChoice lowercase, then capitalizes the
    first letter. It isn't incorrect, but I'm supposed to simply make
    humanChoice's prompt case insensitive instead.

--- game.js
+++ game.js
@@ -39,12 +39,6 @@ getComputerChoice();
 function getHumanChoice(humanChoice) {
     humanChoice = prompt("Rock, Paper, Scissors, SHOOT!", "")
     // INIT humanChoice by prompting for it
-    humanChoice = humanChoice.toLowerCase();
-    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
-    console.log(humanChoice);
-    // MAKE humanChoice input always convert string to lowercase, then
-    // capitalize the first letter. This makes it to where the input is
-    // always output in the same way.
 }
 
 getHumanChoice();
